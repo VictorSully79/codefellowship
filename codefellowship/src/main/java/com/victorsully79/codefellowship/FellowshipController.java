6e5e738101a56bd351cdd3a888b2b39115274317
VictorSully79 <victorsullivan@hotmail.com>
2021-04-13 07:13:41 -0700

    started security and spent a ton of time trying to get my psql to work.

--- codefellowship/src/main/java/com/victorsully79/codefellowship/FellowshipController.java
+++ codefellowship/src/main/java/com/victorsully79/codefellowship/FellowshipController.java
@@ -1,4 +1,19 @@
 package com.victorsully79.codefellowship;
 
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
 public class FellowshipController {
+    @GetMapping("/")
+    public String showFellowshipHome(Principal p) {
+        if (p != null) {
+
+        }
+        return "index";
+    }
 }
---
+++ codefellowship/src/main/java/com/victorsully79/codefellowship/FellowshipRepository.java
@@ -0,0 +1,6 @@
+package com.victorsully79.codefellowship;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FellowshipRepository extends JpaRepository  <FellowShipManager, Long>{
+}
